734bf2a76eb14b6f36e299fad7888c601376f250
neta-dev <36772518+neta-dev@users.noreply.github.com>
2020-02-08 15:06:46 -0500

    Add files via upload

---
+++ backstop_data/html_report/config.js
@@ -0,0 +1,156 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\htmlcss_carvedrock_Utility_Classes_0_nav_0_desktop.png",
+        "test": "..\\bitmaps_test\\20200208-145355\\htmlcss_carvedrock_Utility_Classes_0_nav_0_desktop.png",
+        "selector": "nav",
+        "fileName": "htmlcss_carvedrock_Utility_Classes_0_nav_0_desktop.png",
+        "label": "Utility Classes",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "./index.html",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -293
+          },
+          "misMatchPercentage": "9.68",
+          "analysisTime": 64
+        },
+        "diffImage": "..\\bitmaps_test\\20200208-145355\\failed_diff_htmlcss_carvedrock_Utility_Classes_0_nav_0_desktop.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\htmlcss_carvedrock_Utility_Classes_1_navsection_0_desktop.png",
+        "test": "..\\bitmaps_test\\20200208-145355\\htmlcss_carvedrock_Utility_Classes_1_navsection_0_desktop.png",
+        "selector": "nav + section",
+        "fileName": "htmlcss_carvedrock_Utility_Classes_1_navsection_0_desktop.png",
+        "label": "Utility Classes",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "./index.html",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -14
+          },
+          "misMatchPercentage": "3.26",
+          "analysisTime": 31
+        },
+        "diffImage": "..\\bitmaps_test\\20200208-145355\\failed_diff_htmlcss_carvedrock_Utility_Classes_1_navsection_0_desktop.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\htmlcss_carvedrock_Utility_Classes_2_navsectiondivsectionnth-of-type1_0_desktop.png",
+        "test": "..\\bitmaps_test\\20200208-145355\\htmlcss_carvedrock_Utility_Classes_2_navsectiondivsectionnth-of-type1_0_desktop.png",
+        "selector": "nav + section + div > section:nth-of-type(1)",
+        "fileName": "htmlcss_carvedrock_Utility_Classes_2_navsectiondivsectionnth-of-type1_0_desktop.png",
+        "label": "Utility Classes",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "./index.html",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": -110,
+            "height": -332
+          },
+          "misMatchPercentage": "15.00",
+          "analysisTime": 63
+        },
+        "diffImage": "..\\bitmaps_test\\20200208-145355\\failed_diff_htmlcss_carvedrock_Utility_Classes_2_navsectiondivsectionnth-of-type1_0_desktop.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\htmlcss_carvedrock_Utility_Classes_3_navsectiondivsectionnth-of-type2_0_desktop.png",
+        "test": "..\\bitmaps_test\\20200208-145355\\htmlcss_carvedrock_Utility_Classes_3_navsectiondivsectionnth-of-type2_0_desktop.png",
+        "selector": "nav + section + div > section:nth-of-type(2)",
+        "fileName": "htmlcss_carvedrock_Utility_Classes_3_navsectiondivsectionnth-of-type2_0_desktop.png",
+        "label": "Utility Classes",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "./index.html",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": -110,
+            "height": -1863
+          },
+          "misMatchPercentage": "21.36",
+          "analysisTime": 86
+        },
+        "diffImage": "..\\bitmaps_test\\20200208-145355\\failed_diff_htmlcss_carvedrock_Utility_Classes_3_navsectiondivsectionnth-of-type2_0_desktop.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\htmlcss_carvedrock_Utility_Classes_4_navsectiondivsectionnth-of-type3_0_desktop.png",
+        "test": "..\\bitmaps_test\\20200208-145355\\htmlcss_carvedrock_Utility_Classes_4_navsectiondivsectionnth-of-type3_0_desktop.png",
+        "selector": "nav + section + div > section:nth-of-type(3)",
+        "fileName": "htmlcss_carvedrock_Utility_Classes_4_navsectiondivsectionnth-of-type3_0_desktop.png",
+        "label": "Utility Classes",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "./index.html",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 110,
+            "height": -12
+          },
+          "misMatchPercentage": "4.53",
+          "analysisTime": 30
+        },
+        "diffImage": "..\\bitmaps_test\\20200208-145355\\failed_diff_htmlcss_carvedrock_Utility_Classes_4_navsectiondivsectionnth-of-type3_0_desktop.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\htmlcss_carvedrock_Utility_Classes_5_footer_0_desktop.png",
+        "test": "..\\bitmaps_test\\20200208-145355\\htmlcss_carvedrock_Utility_Classes_5_footer_0_desktop.png",
+        "selector": "footer",
+        "fileName": "htmlcss_carvedrock_Utility_Classes_5_footer_0_desktop.png",
+        "label": "Utility Classes",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "./index.html",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -289
+          },
+          "misMatchPercentage": "2.71",
+          "analysisTime": 52
+        },
+        "diffImage": "..\\bitmaps_test\\20200208-145355\\failed_diff_htmlcss_carvedrock_Utility_Classes_5_footer_0_desktop.png"
+      },
+      "status": "fail"
+    }
+  ],
+  "id": "htmlcss_carvedrock"
+});
